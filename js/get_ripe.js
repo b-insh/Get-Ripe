870800b26631081dbd74c851dd5d156fb70e9bb6
b-insh <bethanyhyland29@gmail.com>
2016-12-20 21:43:36 -0500

    Setup page

--- js/get_ripe.js
+++ js/get_ripe.js
@@ -1,4 +1,57 @@
+const init = (event) => {
+  createScene();
+  // createLights();
+  // createTree();
+  // createMango();
+  // createWateringCan();
+  // createSky();
 
-document.addEventListener("DOMContentLoaded", () => {
-   const world = document.getElementById("world");
-});
+  // loop();
+};
+
+let scene, camera, fieldOfView, aspectRatio, HEIGHT, WIDTH, renderer, container;
+
+const createScene = () => {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+
+  scene = new THREE.Scene();
+
+  aspectRatio = WIDTH / HEIGHT;
+  fieldOfView = 60;
+  camera = new THREE.PerspectiveCamera(
+    fieldOfView,
+    aspectRatio
+  );
+
+  camera.position.x = 0;
+  camera.position.z = 200;
+  camera.position.y = 100;
+
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  });
+
+  renderer.setSize(WIDTH, HEIGHT);
+
+  renderer.shadowMap.enabled = true;
+
+  container = document.getElementById('world');
+  container.appendChild(renderer.domElement);
+
+  window.addEventListener('resize', handleWindowResize, false);
+};
+
+const handleWindowResize = () => {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+  renderer.setSize(WIDTH, HEIGHT);
+  camera.aspect = WIDTH / HEIGHT;
+  camera.updateProjectionMatrix();
+};
+
+
+
+
+window.addEventListener('load', init, false);
